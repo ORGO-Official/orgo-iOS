2d4988183495ef3bb6c68ec1dfb00372468d684a
kth1210 <k2hop1210@gmail.com>
2023-07-07 23:03:21 +0900

    [ADD] : 프로젝트 생성

---
+++ orgo/orgo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
