80592515eecc225f4eacd8d56ffc1b9ae58e461a
kth1210 <k2hop1210@gmail.com>
2023-08-15 22:36:00 +0900

    [ADD] : LoginVC 생성

---
+++ orgo/orgo/Screen/Login/ViewController/LoginVC.swift
@@ -0,0 +1,44 @@
+//
+//  LoginVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/08/15.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+
+class LoginVC: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+    }
+    
+    // MARK: - Functions
+    
+}
