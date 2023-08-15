a4c130835af86a43941b0da2473d8228c03feb29
kth1210 <k2hop1210@gmail.com>
2023-08-15 23:00:19 +0900

    [ADD] : OrgoTabBarVC 생성

---
+++ orgo/orgo/Screen/Common/TabBar/ViewController/OrgoTabBarVC.swift
@@ -0,0 +1,43 @@
+//
+//  OrgoTabBarVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/08/15.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+class OrgoTabBarVC: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+    }
+    
+    // MARK: - Functions
+    
+}
