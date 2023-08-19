c664dbbdb5d6a323858bffc83706cd5922cfe8f6
kth1210 <k2hop1210@gmail.com>
2023-08-19 21:53:36 +0900

    [ADD] : MountainInfoView 생성

---
+++ orgo/orgo/Screen/Home/View/MountainInfoView.swift
@@ -0,0 +1,35 @@
+//
+//  MountainInfoView.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/08/19.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class MountainInfoView: BaseView {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func configureView() {
+        super.configureView()
+        
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+    }
+    
+    // MARK: - Functions
+    
+}
