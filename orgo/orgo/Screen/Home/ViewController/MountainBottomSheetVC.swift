d0f251703f5f872cbd0c5c8bd091620ab51b12aa
kth1210 <k2hop1210@gmail.com>
2023-08-19 21:52:28 +0900

    [ADD] : MountainBottomSheetVC 생성

---
+++ orgo/orgo/Screen/Home/ViewController/MountainBottomSheetVC.swift
@@ -0,0 +1,44 @@
+//
+//  MountainBottomSheetVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/08/19.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxGesture
+import RxCocoa
+
+class MountainBottomSheetVC: OrgoBottomSheet {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+    }
+    
+    // MARK: - Functions
+    
+}
