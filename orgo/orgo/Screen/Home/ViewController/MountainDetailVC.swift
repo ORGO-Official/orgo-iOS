2dd4251ac9e0d14bad705b4189742d7b4c2f34db
kth1210 <k2hop1210@gmail.com>
2023-08-21 23:50:07 +0900

    [ADD] : MountainDetailVC 생성

---
+++ orgo/orgo/Screen/Home/ViewController/MountainDetailVC.swift
@@ -0,0 +1,67 @@
+//
+//  MountainDetailVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/08/21.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+class MountainDetailVC: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension MountainDetailVC {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension MountainDetailVC {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
