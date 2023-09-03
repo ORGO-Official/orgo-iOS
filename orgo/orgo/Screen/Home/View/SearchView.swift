e9cd6c318346796d6c810bb7e5670b28df6e0f52
kth1210 <k2hop1210@gmail.com>
2023-09-03 22:32:30 +0900

    [ADD] : SearchView 생성

---
+++ orgo/orgo/Screen/Home/View/SearchView.swift
@@ -0,0 +1,59 @@
+//
+//  SearchView.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/03.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class SearchView: BaseView {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension SearchView {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension SearchView {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
