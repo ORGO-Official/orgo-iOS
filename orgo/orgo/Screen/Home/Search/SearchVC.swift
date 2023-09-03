a274315f8b816fb7eef247378e78a80361e1afaa
kth1210 <k2hop1210@gmail.com>
2023-09-03 21:40:12 +0900

    [ADD] : SearchVC 생성

---
+++ orgo/orgo/Screen/Home/Search/SearchVC.swift
@@ -0,0 +1,67 @@
+//
+//  SearchVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/03.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+class SearchVC: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension SearchVC {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension SearchVC {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
