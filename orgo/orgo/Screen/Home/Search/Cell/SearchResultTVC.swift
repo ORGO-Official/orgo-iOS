095a05a0eb9688c01e49ef80f2b2c4ad3854e4d8
kth1210 <k2hop1210@gmail.com>
2023-09-04 00:08:09 +0900

    [ADD] : SearchResultTVC 생성

---
+++ orgo/orgo/Screen/Home/Search/Cell/SearchResultTVC.swift
@@ -0,0 +1,66 @@
+//
+//  SearchResultTVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/04.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxGesture
+import RxCocoa
+
+class SearchResultTVC: BaseTableViewCell {
+    
+    // MARK: - UI components
+        
+    // MARK: - Variables and Properties
+    
+    // MARK: - Life Cycle
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureContentView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+
+    // MARK: - Function
+    
+}
+
+
+// MARK: - Confgure
+
+extension SearchResultTVC {
+    
+    private func configureContentView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension SearchResultTVC {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
