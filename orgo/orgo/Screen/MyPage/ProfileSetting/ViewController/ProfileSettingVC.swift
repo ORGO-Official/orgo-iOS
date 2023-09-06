f95d5496b623e59e38c9380568a96d54f858f2b6
kth1210 <k2hop1210@gmail.com>
2023-09-07 01:47:07 +0900

    [ADD] : ProfileSettingVC 생성

---
+++ orgo/orgo/Screen/MyPage/ProfileSetting/ViewController/ProfileSettingVC.swift
@@ -0,0 +1,67 @@
+//
+//  ProfileSettingVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/07.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+class ProfileSettingVC: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension ProfileSettingVC {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension ProfileSettingVC {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
