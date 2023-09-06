0c34448cc5197dd2e7d5c043d4d564a3a9ac8751
kth1210 <k2hop1210@gmail.com>
2023-09-07 01:15:53 +0900

    [ADD] : UserInfoView 생성

---
+++ orgo/orgo/Screen/MyPage/View/UserInfoView.swift
@@ -0,0 +1,59 @@
+//
+//  UserInfoView.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/07.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class UserInfoView: BaseView {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension UserInfoView {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension UserInfoView {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
