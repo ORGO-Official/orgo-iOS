c19655ac0e29721938258a9b8919e1d621132562
kth1210 <k2hop1210@gmail.com>
2023-09-15 02:05:58 +0900

    [CHORE] : 산 아이콘 추가

--- orgo/orgo/Global/Assets/ImageAssets.swift
+++ orgo/orgo/Global/Assets/ImageAssets.swift
@@ -9,14 +9,20 @@ import UIKit
 
 enum ImageAssets {
     
+    // MARK: - Logo
+    
     static let orgoLogoGreen = UIImage(named: "OrgoLogoGreen")?.withRenderingMode(.alwaysOriginal)
     static let orgoLogoWhite = UIImage(named: "OrgoLogoWhite")?.withRenderingMode(.alwaysOriginal)
+    
+    // MARK: - Login
     
     static let kakaoLogin = UIImage(named: "KakaoLogin")?.withRenderingMode(.alwaysOriginal)
     static let naverLogin = UIImage(named: "NaverLogin")?.withRenderingMode(.alwaysOriginal)
     static let appleLogin = UIImage(named: "AppleLogin")?.withRenderingMode(.alwaysOriginal)
     static let loginNext = UIImage(named: "LoginNext")?.withRenderingMode(.alwaysOriginal)
     static let loginBorder = UIImage(named: "LoginBorder")?.withRenderingMode(.alwaysOriginal)
+    
+    // MARK: - Common
     
     static let selectedHouse = UIImage(named: "SelectedHouse")?.withRenderingMode(.alwaysOriginal)
     static let unselectedHouse = UIImage(named: "UnselectedHouse")?.withRenderingMode(.alwaysOriginal)
@@ -25,10 +31,20 @@ enum ImageAssets {
     
     static let backButton = UIImage(named: "BackButton")?.withRenderingMode(.alwaysOriginal)
     
+    // MARK: - Mountain
+    
     static let mountainMarker = UIImage(named: "MountainMarker")?.withRenderingMode(.alwaysOriginal)
+    static let mountain200Marker = UIImage(named: "Mountain200")?.withRenderingMode(.alwaysOriginal)
+    static let mountain400Marker = UIImage(named: "Mountain400")?.withRenderingMode(.alwaysOriginal)
+    static let mountain600Marker = UIImage(named: "Mountain600")?.withRenderingMode(.alwaysOriginal)
+    static let mountain800Marker = UIImage(named: "Mountain00")?.withRenderingMode(.alwaysOriginal)
+    
+    // MARK: - Search
     
     static let location = UIImage(named: "Location")?.withRenderingMode(.alwaysOriginal)
     static let roundExclamation = UIImage(named: "RoundExclamation")?.withRenderingMode(.alwaysOriginal)
+    
+    // MARK: - Setting
     
     static let setting = UIImage(named: "Setting")?.withRenderingMode(.alwaysOriginal)
 }
