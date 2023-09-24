c395f15155ba414b8770c197776be2b7cb3e4a66
kth1210 <k2hop1210@gmail.com>
2023-09-24 21:29:42 +0900

    [ADD] : OrgoPopUpVC 생성

---
+++ orgo/orgo/Screen/Common/PopUp/ViewController/OrgoPopUpVC.swift
@@ -0,0 +1,69 @@
+//
+//  OrgoPopUpVC.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+import RxGesture
+
+
+class OrgoPopUpVC: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension OrgoPopUpVC {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension OrgoPopUpVC {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
