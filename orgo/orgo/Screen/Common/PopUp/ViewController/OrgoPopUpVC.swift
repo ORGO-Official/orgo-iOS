5fcf93477ead0f3eb720f4c8c22fbd605e600fff
kth1210 <k2hop1210@gmail.com>
2023-09-24 21:56:07 +0900

    [FEAT] : OrgoPopUpVC UI components 추가

--- orgo/orgo/Screen/Common/PopUp/ViewController/OrgoPopUpVC.swift
+++ orgo/orgo/Screen/Common/PopUp/ViewController/OrgoPopUpVC.swift
@@ -19,6 +19,64 @@ class OrgoPopUpVC: BaseViewController {
     
     // MARK: - UI components
     
+    private let dimmedView: UIView = UIView()
+        .then {
+            $0.backgroundColor = .black.withAlphaComponent(0.7)
+        }
+    
+    private let contentView: UIView = UIView()
+        .then {
+            $0.layer.cornerRadius = 5.0
+            $0.backgroundColor = .white
+        }
+    
+    private let descView: UIView = UIView()
+    
+    private let descStackView: UIStackView = UIStackView()
+        .then {
+            $0.axis = .vertical
+            $0.spacing = 4.0
+            $0.alignment = .fill
+            $0.distribution = .fill
+        }
+    
+    private let titleLabel: UILabel = UILabel()
+        .then {
+            $0.textAlignment = .center
+            $0.font = UIFont.pretendard(size: 14.0, weight: .medium)
+            $0.textColor = .black
+        }
+    
+    private let descLabel: UILabel = UILabel()
+        .then {
+            $0.textAlignment = .center
+            $0.font = UIFont.pretendard(size: 14.0, weight: .medium)
+            $0.textColor = .black
+            $0.numberOfLines = 0
+        }
+    
+    private let btnStackView: UIStackView = UIStackView()
+        .then {
+            $0.axis = .horizontal
+            $0.spacing = 10.0
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+        }
+    
+    private let confirmBtn: UIButton = UIButton(type: .system)
+        .then {
+            $0.setBackgroundColor(ColorAssets.lightGray, for: .normal)
+            $0.setBackgroundColor(.systemBlue, for: .highlighted)
+            $0.layer.cornerRadius = 5.0
+        }
+    
+    private let cancelBtn: UIButton = UIButton(type: .system)
+        .then {
+            $0.setBackgroundColor(ColorAssets.lightGray, for: .normal)
+            $0.setBackgroundColor(.systemBlue, for: .highlighted)
+            $0.layer.cornerRadius = 5.0
+        }
+    
     
     // MARK: - Variables and Properties
     
@@ -52,7 +110,20 @@ class OrgoPopUpVC: BaseViewController {
 extension OrgoPopUpVC {
     
     private func configureInnerView() {
+        view.addSubviews([dimmedView,
+                          contentView])
         
+        contentView.addSubviews([descView, btnStackView])
+        
+        descView.addSubviews([descStackView])
+        
+        [titleLabel, descLabel].forEach {
+            descStackView.addArrangedSubview($0)
+        }
+        
+        [confirmBtn, cancelBtn].forEach {
+            btnStackView.addArrangedSubview($0)
+        }
     }
     
 }
@@ -63,7 +134,30 @@ extension OrgoPopUpVC {
 extension OrgoPopUpVC {
     
     private func configureLayout() {
+        dimmedView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
+        contentView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16.0)
+            $0.height.equalTo(contentView.snp.width).dividedBy(2.0)
+        }
+        
+        descView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(btnStackView.snp.top)
+        }
+        
+        descStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(40.0)
+        }
+        
+        btnStackView.snp.makeConstraints {
+            $0.height.equalTo(contentView.snp.height).dividedBy(5.2)
+            $0.leading.bottom.trailing.equalToSuperview().inset(16.0)
+        }
     }
     
 }
