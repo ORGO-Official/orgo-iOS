0f2a74a642058a8e82ad5553142b9d07f1bbf825
kth1210 <k2hop1210@gmail.com>
2023-09-30 23:40:42 +0900

    [ADD] : MenuBoxView 생성

---
+++ orgo/orgo/Screen/Common/RecordCompletion/MenuBoxView.swift
@@ -0,0 +1,57 @@
+//
+//  MenuBoxView.swift
+//  orgo
+//
+//  Created by 김태현 on 2023/09/30.
+//
+
+import Foundation
+
+
+class MenuBoxView: BaseView {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureInnerView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension MenuBoxView {
+    
+    private func configureInnerView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension MenuBoxView {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
